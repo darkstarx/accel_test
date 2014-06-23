1cae01143812c70f3d3a184aa51b51a167863b59
Sergey Elohin <s.elohin@socialquantum.ru>
2014-06-23 20:55:12 +0400

    Какой-то фикс для сборки и запуска в свежей студии

--- accelTest/build/source/buildConfig/debug/com/sq/acceltest/BuildConfig.java
+++ accelTest/build/source/buildConfig/debug/com/sq/acceltest/BuildConfig.java
@@ -1,7 +1,12 @@
-/** Automatically generated file. DO NOT MODIFY */
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
 package com.sq.acceltest;
 
 public final class BuildConfig {
-    public final static boolean DEBUG = true;
-
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String PACKAGE_NAME = "com.sq.acceltest";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
 }
